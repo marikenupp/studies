6a7563bfc40ae270461a5a19b9c39740028d8f64
marikenupp <marianakenupp@icloud.com>
2023-05-13 22:39:06 -0300

    API AdressValidation

--- requests/api.py
+++ requests/api.py
@@ -6,13 +6,9 @@ ListPostalCode = ['01153000','20050000','70714020']
 for PostalCode in ListPostalCode:
 
     URL     = 'https://viacep.com.br/ws/{}/json/'.format(PostalCode)
-
     Request = requests.get(URL)
-
     Adress  = Request.json()
-
     # print(tAdress) 
-
     ListAdress.append([Adress["cep"],Adress["logradouro"],Adress["complemento"],Adress["uf"]])
 
 for ListAdres in ListAdress:
